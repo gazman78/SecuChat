1839bdecbbf091988687365e390dc615df0e4de8
Spl3en <spl3en.contact@gmail.com>
2015-01-23 11:36:40 +0100

    Refactoring, fix mail check. Add an array of clients.

--- server.js
+++ server.js
@@ -1,33 +1,55 @@
-var net = require('net');
-var fs = require("fs");
-var readline = require('readline');
-var ibe = require('./ibe/ibe_extract.js');
-var server = net.createServer(function(c) {
-	console.log('client connected');
-	c.on('end', function() {
-		console.log('client disconnected');
+// Dependencies
+var net = require ('net');
+var fs = require ("fs");
+var readline = require ('readline');
+var ibe = require ('./ibe/ibe_extract.js');
+
+// Global array of clients
+var clients = {};
+
+var server = net.createServer (function (client) {
+	
+	// The server received a connection from a client
+	console.log ('client connected');
+	clients.push (client);
+
+	client.on ('end', function () {
+		// Callback called after a client connection termination
+		console.log ('client disconnected');
+		if ((clientIndex = clients.indexOf (client)) > -1) {
+			// Remove the client from the clients array
+			clients.splice (clientIndex, 1);
+		}
 	});
-	c.on('data', function(data){
-		var contenu = fs.readFileSync("clients_already_exist", "UTF-8");
-		if(contenu.indexOf(data) > -1) {
-			console.log('client already defined');
+
+	client.on ('data', function (data) {
+		// The server received data from a client
+		var contenuFichierClients = fs.readFileSync ("clients_already_exist", "UTF-8");
+		if (contenuFichierClients.indexOf (data) > -1) {
+			/*! FIXBUG
+				Si un client s'enregistre avec le nom "alut@gmail.com",
+				l'email d'un autre client "salut@gmail.com" sera refusé.
+			*/
+			console.log ('client already defined');
 		}
 		else {
 			// Check email format 
-			var regEmail = new RegExp('^[0-9a-z._-]+@{1}[0-9a-z.-]{2,}[.]{1}[a-z]{2,5}$','i');
-			if (!regEmail.test(myVar)) {
+			if (!(new RegExp ('^[0-9a-z._-]+@{1}[0-9a-z.-]{2,}[.]{1}[a-z]{2,5}$','i').test (data))) {
+				// Error : mail not correctly formated
 				console.log ("Wrong email address");
 			}
 			else {
-				var sercret_key=ibe.extract_secret_key(data);
-				fs.appendFile("clients_already_exist", data, "UTF-8");
-				console.log(sercret_key);
-				c.write(sercret_key);
-			]
+				// Generate a secret_key
+				var secret_key = ibe.extract_secret_key (data);
+				fs.appendFile ("clients_already_exist", data, "UTF-8");
+				// Send the secret_key to the client
+				client.write (secret_key);
+			}
 		}
-	});	
-	c.pipe(c);
+	});
 });
-server.listen(8124, function() {
-	console.log('server listening on port 8124');
+
+// Bind port 8124 and listen for connections
+server.listen (8124, function () {
+	console.log ('server listening on port 8124');
 });
