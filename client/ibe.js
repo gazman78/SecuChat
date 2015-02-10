dc25bbece6ee958ab3757bf5a940546fb077e4f6
Philippe JOULOT <philippe.joulot@laposte.net>
2015-02-10 15:35:23 +0100

    IBE_decrypt

--- client/ibe.js
+++ client/ibe.js
@@ -1,6 +1,6 @@
 var Nmax = 4096;
 
-function IBE_Encrypt(C, const m, id0, MPKD, RD)
+function IBE_Encrypt(C, m, id0, MPKD, RD)
 {
 		var i, N, q0;
 		var r = new Array(Nmax);
@@ -38,4 +38,31 @@ function IBE_Encrypt(C, const m, id0, MPKD, RD)
 			C[0][i] = (C[0][i] + e1[i] + q0/2)%q0 - (q0/2);
 			C[1][i] = (C[1][i] + e2[i] + (q0/2)*m[i] + q0/2)%q0 - (q0/2);
 		}
+}
+
+function IBE_Decrypt(w0, C, SKid_FFT, RD)
+{
+	var i, N, q0;
+	var c0_FFT[Nmax]  = new Array(Nmax);
+	var aux_FFT[Nmax]  = new Array(Nmax);
+	
+	q0 = RD["q0"];
+	N = RD["N"];
+	
+	MyIntFFT(c0_FFT, C[0], N);
+	
+	for(i=0; i<N; i++)
+	{
+		aux_FFT[i] = c0_FFT[i].mul(SKid_FFT[i]);
+	}
+	
+	MyIntReverseFFT(w0, aux_FFT, N);
+	
+	for(i=0; i<N; i++)
+	{
+		w0[i] = C[1][i] - w0[i];
+		w0[i] = (w0[i])%q0;
+		w0[i] = (w0[i] + (q0>>2) )/(q0>>1);
+		w0[i] %= 2;
+	}
 }
